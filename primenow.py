d8e68c7f455e0a6a396349b05cf23292be53a1f3
John Theodore <johntheodore@github.gov>
2020-04-06 13:58:58 -0700

    everything works for the POC. I can automate orders with primenow

--- primenow.py
+++ primenow.py
@@ -1,6 +1,5 @@
 import datetime
 import io
-import ipdb
 import json
 import re
 import requests
@@ -10,10 +9,10 @@ from bs4 import BeautifulSoup
 from pycookiecheat import chrome_cookies
 from pygame import mixer
 
-
 primenow_url = 'https://primenow.amazon.com'
 headers = {
-    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) \
+  'User-Agent':
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) \
     Chrome/80.0.3987.149 Safari/537.36',
 }
 
@@ -26,7 +25,11 @@ def query_primenow(url, cookies, method='get', data=None, params=None):
   if method == 'get':
     response = requests.get(url, headers=headers, cookies=cookies)
   elif method == 'post':
-    response = requests.post(url, headers=headers, cookies=cookies, params=params, data=data)
+    response = requests.post(url,
+                             headers=headers,
+                             cookies=cookies,
+                             params=params,
+                             data=data)
   response.raise_for_status()
   if 'Sign in' in str(response.content):
     sys.exit('Invalid authentication.')
@@ -40,16 +43,20 @@ def exit_on_empty_shopping_cart(checkout_button_html):
 
 def get_checkout_html():
   primenow_cookies = get_primenow_cookies()
-  shopping_cart_response = query_primenow('https://primenow.amazon.com/cart', primenow_cookies)
+  shopping_cart_response = query_primenow('https://primenow.amazon.com/cart',
+                                          primenow_cookies)
   primenow_cookies.update(shopping_cart_response.cookies.get_dict())
-  shopping_cart_html = BeautifulSoup(shopping_cart_response.content, features='html.parser')
-  checkout_button_html = shopping_cart_html.findAll('span', {'class': 'cart-checkout-button'})
+  shopping_cart_html = BeautifulSoup(shopping_cart_response.content,
+                                     features='html.parser')
+  checkout_button_html = shopping_cart_html.findAll(
+    'span', {'class': 'cart-checkout-button'})
   exit_on_empty_shopping_cart(checkout_button_html)
   checkout_link = checkout_button_html[0].find('a')['href']
   checkout_url = '%s%s' % (primenow_url, checkout_link)
   checkout_response = query_primenow(checkout_url, primenow_cookies)
   primenow_cookies.update(checkout_response.cookies.get_dict())
-  return (BeautifulSoup(checkout_response.content, features='html.parser'), primenow_cookies)
+  return (BeautifulSoup(checkout_response.content,
+                        features='html.parser'), primenow_cookies)
 
 
 def get_earliest_delivery_window(checkout_html):
@@ -58,47 +65,63 @@ def get_earliest_delivery_window(checkout_html):
     print('empty delivery box div again?')
     buy_primenow_groceries()
   two_hour_soup_block = two_hour_soup_block[0]
-  delivery_times = two_hour_soup_block.findAll('div', {'class': 'a-section a-spacing-none'})
+  delivery_times = two_hour_soup_block.findAll(
+    'div', {'class': 'a-section a-spacing-none'})
   available_delivery_windows = []
   for delivery_time in delivery_times:
-    time_slot = delivery_time.findAll(text=re.compile('M \('))[0].strip()
-    delivery_key_html = delivery_time.find(attrs={'data-action': 'selectdeliverywindow'})['data-selectdeliverywindow']
+    time_slot = delivery_time.findAll(text=re.compile(' - '))[0].strip()
+    delivery_key_html = delivery_time.find(
+      attrs={'data-action': 'selectdeliverywindow'
+            })['data-selectdeliverywindow']
     delivery_json = json.loads(delivery_key_html)
-    available_delivery_windows.append({'time_slot': time_slot, 'delivery_json': delivery_json})
+    available_delivery_windows.append({
+      'time_slot': time_slot,
+      'delivery_json': delivery_json
+    })
   print(available_delivery_windows)
   return available_delivery_windows[0]
 
 
-def set_earliest_delivery_window(checkout_html, delivery_window, primenow_cookies):
-  next_url = checkout_html.findAll('form', {'action' : re.compile('/checkout/deliveryslot/')})[0]['action'].split('&')[0]
-  delivery_slot_form = checkout_html.findAll('form', {'name' : 'deliverySlotForm'})[0]
-  token_value = delivery_slot_form.findAll('input', {'name': 'tokenValue'})[0]['value']
+def set_earliest_delivery_window(checkout_html, delivery_window,
+                                 primenow_cookies):
+  next_url = checkout_html.findAll(
+    'form', {'action': re.compile('/checkout/deliveryslot/')
+            })[0]['action'].split('&')[0]
+  delivery_slot_form = checkout_html.findAll('form',
+                                             {'name': 'deliverySlotForm'})[0]
+  token_value = delivery_slot_form.findAll('input',
+                                           {'name': 'tokenValue'})[0]['value']
   params = (
-      ('nexturl', next_url),
-      ('ref_', 'pn_co_ds_c'),
-      ('fromPanel', 'delivery-slot'),
+    ('nexturl', next_url),
+    ('ref_', 'pn_co_ds_c'),
+    ('fromPanel', 'delivery-slot'),
   )
   data = delivery_window['delivery_json']
   data.update({
     'tokenName': 'delivery-slot',
     'shipOptionType': 'scheduled_two_hour_delivery',
     'sameDay': '',
-    'deliveryType': 'UNATTENDED'}
-  )
+    'deliveryType': 'UNATTENDED'
+  })
   data['tokenValue'] = token_value
   checkout_prefetch_url = 'https://primenow.amazon.com/checkout/prefetch'
-  response = query_primenow(checkout_prefetch_url, primenow_cookies, method='post', params=params, data=data)
+  response = query_primenow(checkout_prefetch_url,
+                            primenow_cookies,
+                            method='post',
+                            params=params,
+                            data=data)
   return response
 
 
-def continue_to_latest_delivery(set_delivery_window_response, primenow_cookies):
-  params = (
-    ('ref_', 'pn_co_ot_po'),
-  )
+def purchase_and_ship_cart(set_delivery_window_response, primenow_cookies):
+  params = (('ref_', 'pn_co_ot_po'),)
   data = {}
-  continue_html_string = json.loads(set_delivery_window_response.content)['htmlResponse']
-  continue_html_soup = BeautifulSoup(continue_html_string, features='html.parser')
-  continue_data_html = continue_html_soup.findAll('form', {'action': '/checkout/spc/continue?ref_=pn_co_ot_po'})[0]
+  continue_html_string = json.loads(
+    set_delivery_window_response.content)['htmlResponse']
+  continue_html_soup = BeautifulSoup(continue_html_string,
+                                     features='html.parser')
+  continue_data_html = continue_html_soup.findAll(
+    'form', {'action': '/checkout/spc/continue?ref_=pn_co_ot_po'})[0]
   data_inputs = continue_data_html.findAll('input')
   for data_input in data_inputs:
     name = data_input.get('name')
@@ -106,32 +129,32 @@ def continue_to_latest_delivery(set_delivery_window_response, primenow_cookies):
       data[name] = data_input['value']
 
   continue_url = 'https://primenow.amazon.com/checkout/spc/continue'
-  # cookies = set_delivery_window_response.cookies.get_dict()
-  ipdb.set_trace()
-  response = query_primenow(continue_url, primenow_cookies, method='post', params=params, data=data)
-  # response = query_primenow(continue_url, cookies, method='post', params=params, data=data)
+  response = query_primenow(continue_url,
+                            primenow_cookies,
+                            method='post',
+                            params=params,
+                            data=data)
   return response
-
-
-def purchase_and_ship_cart(continue_to_delivery_response):
-  ipdb.set_trace()
-  return True
 
 
 def checkout(checkout_html, primenow_cookies):
   delivery_window = get_earliest_delivery_window(checkout_html)
   play_victory_music()
   time.sleep(3)
-  set_delivery_window_response = set_earliest_delivery_window(checkout_html, delivery_window, primenow_cookies)
-  continue_to_delivery_response = continue_to_latest_delivery(set_delivery_window_response, primenow_cookies)
-  purchase_and_ship_cart(continue_to_delivery_response)
+  set_delivery_window_response = set_earliest_delivery_window(
+    checkout_html, delivery_window, primenow_cookies)
+  # this response returned below is a javascript payload.
+  # would need to hit order status endpoint to verify order
+  purchase_and_ship_cart(set_delivery_window_response, primenow_cookies)
 
 
 def is_delivery_time_available(checkout_html):
-  no_delivery_time = checkout_html.findAll(text=re.compile('No delivery windows available.'))
+  no_delivery_time = checkout_html.findAll(
+    text=re.compile('No delivery windows available.'))
   if len(no_delivery_time) > 0:
     return False
-  wants_address_selected = checkout_html.findAll(text=re.compile('Select Delivery Address'))
+  wants_address_selected = checkout_html.findAll(
+    text=re.compile('Select Delivery Address'))
   if len(wants_address_selected) > 0:
     print('dumb address bug')
     return False
@@ -139,7 +162,8 @@ def is_delivery_time_available(checkout_html):
 
 
 def play_victory_music():
-  daft_punk = requests.get('https://download.mp3-here.icu/i/Daft-Punk-End-Of-Line.mp3')
+  daft_punk = requests.get(
+    'https://download.mp3-here.icu/i/Daft-Punk-End-Of-Line.mp3')
   mixer.init()
   mixer.music.load(io.BytesIO(daft_punk.content))
   mixer.music.play()
